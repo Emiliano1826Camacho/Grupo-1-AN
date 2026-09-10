3fca57308359837becf853d72a3107d43bf254a3
EmilioAvelar <00239526@uca.edu.sv>
2026-09-10 14:19:51 -0600

    Agregar funcion Consultar calorias y agregar calorias

--- calorias_diarias.cc
+++ calorias_diarias.cc
@@ -1,7 +1,23 @@
 #include <iostream>
 
+struct Calorias
+{
+    float calorias;
+};
+// Declaracion de funciones
+void ConsultarCalorias(float calorias);
+void AgregarCalorias(int &calorias, int cant);
 int main()
 {
-
     return 0;
+}
+// Paso por valor
+void ConsultarCalorias(float calorias)
+{
+    std::cout << "Calorias actuales: " << calorias << "CAL" << std::endl;
+}
+// Paso por referencia
+void AgregarCalorias(int &calorias, int cant)
+{
+    calorias += cant;
 }
